668233e3a2c1ecf351d201b7c4fede523b5e2f38
anies1212 <all.for.me1212sna@gmail.com>
2022-06-08 18:25:37 +0900

    Initial Commit

---
+++ NetflixSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixSample
+//
+//  Created by anies1212 on 2022/06/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
